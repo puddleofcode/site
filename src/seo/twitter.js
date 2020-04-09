2cc68437489e3d8e6a641d4028b067c3eeb9c457
Michał Kalbarczyk <fazibear@gmail.com>
2020-04-09 20:20:29 +0200

    Upgrade dependencies

--- src/seo/twitter.js
+++ src/seo/twitter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 export default ({ type = 'summary_large_image', username = null, title, description, image }) => (
   <Helmet>
